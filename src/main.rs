db1ebca0510e0537215b37923b03da1dc1cfba29
Joel Singh <joelsingh788@gmail.com>
2025-07-31 00:10:42 -0400

    Add bevy_svg

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, bevy_svg::prelude::SvgPlugin))
         .add_systems(Startup, setup_camera)
         .run();
 }
