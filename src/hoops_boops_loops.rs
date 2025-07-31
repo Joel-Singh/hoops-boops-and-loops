c0bc8b0f7b316d65dc8fe72349bc010fae8226e2
Joel Singh <joelsingh788@gmail.com>
2025-07-31 09:49:50 -0400

    Spawn loop with boop

--- src/hoops_boops_loops.rs
+++ src/hoops_boops_loops.rs
@@ -10,6 +10,16 @@ struct Boop {
     in_hoop: bool,
 }
 
+impl Default for Boop {
+    fn default() -> Self {
+        Boop {
+            r#loop: Entity::PLACEHOLDER,
+            current_loop_position: Rot2::default(),
+            in_hoop: false,
+        }
+    }
+}
+
 #[derive(Component)]
 struct Hoop {
     r#loop: Entity,
@@ -22,9 +32,13 @@ struct Loop {
 }
 
 const MAX_HOOPS: u32 = 8;
+const MAX_BOOPS: u32 = 16;
 /// The max number of hoop
 const INITIAL_HOOP_POSITION: Rot2 = Rot2::FRAC_PI_8;
 /// The initial position of the first hoop.
+
+const LOOP_FILE_HEIGHT: f32 = 720.;
+const LOOP_RADIUS: f32 = LOOP_FILE_HEIGHT / 2.;
 
 pub fn hoops_boops_loops_plugin(app: &mut App) {
     app.add_systems(
@@ -49,31 +63,75 @@ impl Command for SpawnLoop {
     // Hoops and Loops will be children of the loop (along with being kept track of in Loops.boops/Loops.hoops) so that their transform is relative to the Loop
 
     fn apply(self, world: &mut World) {
-        let _ = world.run_system_cached(
-            |mut commands: Commands, asset_server: ResMut<AssetServer>| {
-                let mut transform: Transform = Transform::default();
-                transform.scale *= 0.15;
-                commands.spawn((
-                    Loop::default(),
-                    Sprite::from_image(load_random_variant("loop", &asset_server, 1, 5)),
-                    transform,
-                ));
-            },
-        );
+        let asset_server = world.get_resource_mut::<AssetServer>().unwrap();
+        let loop_image = load_random_variant("loop", &asset_server, 1, 5);
+
+        let mut commands = world.commands();
+
+        const LOOP_SCALE: f32 = 0.15;
+
+        let r#loop = commands
+            .spawn((
+                Sprite::from_image(loop_image),
+                Transform {
+                    translation: self.0.extend(0.0),
+                    scale: Transform::default().scale * LOOP_SCALE,
+                    ..default()
+                },
+                Loop::default(),
+            ))
+            .id();
+
+        commands.queue(AddBoop(r#loop));
     }
 }
 
 /// Custom EntityCommand that adds a hoop to a loop
 /// The hoop is placed in the INITIAL_HOOP_POSITION, or else, 1/8 from the last one
-/// Does nothing and emits warning if you try to add a hoop to a loop that already has 8
+/// Does nothing and emits warning if you try to add a hoop to a loop that already has MAX_HOOPS. Or if
+/// this is ran on an entity without the Loop component
 pub struct AddHoop;
 impl EntityCommand for AddHoop {
     fn apply(self, entity: EntityWorldMut) {}
 }
 
-pub struct AddBoop;
-impl EntityCommand for AddBoop {
-    fn apply(self, entity: EntityWorldMut) {}
+/// Custom EntityCommand that adds a boop to a loop
+/// panics if you try to add a boop to a loop that already has MAX_BOOPS
+/// Does not check if entity is a loop, behavior is undefined if so
+pub struct AddBoop(Entity);
+impl Command for AddBoop {
+    fn apply(self, world: &mut World) {
+        let r#loop = self.0;
+        let asset_server = world.get_resource_mut::<AssetServer>().unwrap();
+        let boop_image = load_random_variant("boop", &asset_server, 1, 5);
+
+        let mut commands = world.commands();
+
+        const BOOP_SCALE: f32 = 0.1;
+        const BOOP_TO_LOOP_MARGIN: f32 = 70.;
+
+        let new_boop = commands
+            .spawn((
+                Sprite::from_image(boop_image),
+                Transform {
+                    translation: Vec3::default().with_y(LOOP_RADIUS + BOOP_TO_LOOP_MARGIN),
+                    scale: Transform::default().scale * BOOP_SCALE,
+                    ..default()
+                },
+                Boop {
+                    r#loop,
+                    ..default()
+                },
+            ))
+            .id();
+
+        commands
+            .entity(r#loop)
+            .entry::<Loop>()
+            .and_modify(move |mut r#loop| r#loop.boops.push(new_boop));
+
+        commands.entity(r#loop).add_child(new_boop);
+    }
 }
 
 /// Load randomly "file-#.svg" with the # being replaced by a random number from start to end
