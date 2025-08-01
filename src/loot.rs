85491f095f8f29ddc97eff3d9c0b4ced205b1230
Joel Singh <joelsingh788@gmail.com>
2025-07-31 23:00:45 -0400

    Implement temporary loot display

--- src/loot.rs
+++ src/loot.rs
@@ -4,18 +4,27 @@ use bevy::prelude::*;
 #[derive(Resource, Deref, DerefMut)]
 pub struct Loot(i32);
 
+/// Marker struct for the Loot Display Text
+#[derive(Component)]
+struct LootDisplay;
+
 pub fn loot_plugin(app: &mut App) {
-    app.add_systems(
-        FixedUpdate,
-        update_loot_display.run_if(resource_changed::<Loot>),
-    )
-    .insert_resource(Loot(0));
+    app.add_systems(Startup, spawn_display)
+        .add_systems(
+            FixedUpdate,
+            update_loot_display.run_if(resource_changed::<Loot>),
+        )
+        .insert_resource(Loot(0));
 }
 
 /// Show the the loot counter, with bevy easings so it doesn't abruptly show
 pub fn show() {}
 
+pub fn spawn_display(mut commands: Commands) {
+    commands.spawn((LootDisplay, Text::new("0")));
+}
+
 /// Update the loot display
-pub fn update_loot_display(loot: Res<Loot>) {
-    dbg!(**loot);
+pub fn update_loot_display(loot: Res<Loot>, mut text: Single<&mut Text, With<LootDisplay>>) {
+    text.0 = loot.to_string();
 }
