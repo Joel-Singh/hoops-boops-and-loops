d159edd9f2dd268c2f555bf793dd35c7ee9bd92e
Joel Singh <joelsingh788@gmail.com>
2025-08-01 16:27:51 -0400

    Add proper loot display

--- src/loot.rs
+++ src/loot.rs
@@ -4,9 +4,13 @@ use bevy::prelude::*;
 #[derive(Resource, Deref, DerefMut)]
 pub struct Loot(i32);
 
-/// Marker struct for the Loot Display Text
+/// Marker struct for the Loot Display
 #[derive(Component)]
 struct LootDisplay;
+
+/// Marker struct for the entity that contains the Text of the LootDisplay representing current loot
+#[derive(Component)]
+struct CurrentLootText;
 
 pub fn loot_plugin(app: &mut App) {
     app.add_systems(Startup, spawn_display)
@@ -20,11 +24,71 @@ pub fn loot_plugin(app: &mut App) {
 /// Show the the loot counter, with bevy easings so it doesn't abruptly show
 pub fn show() {}
 
-pub fn spawn_display(mut commands: Commands) {
-    commands.spawn((LootDisplay, Text::new("0")));
+pub fn spawn_display(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let loot_symbol = asset_server.load("loot-symbol.png");
+    let spacey_font = asset_server.load("SpaceGrotesk-Light.ttf");
+
+    let loot_display = commands
+        .spawn((
+            LootDisplay,
+            Node {
+                height: Val::Px(50.),
+                border: UiRect::all(Val::Px(1.)),
+                margin: UiRect::left(Val::Auto)
+                    .with_right(Val::Px(50.))
+                    .with_top(Val::Px(30.)),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            BackgroundColor(Color::WHITE.with_alpha(0.1)),
+            BorderColor(Color::BLACK),
+            BorderRadius::MAX,
+        ))
+        .id();
+
+    let loot_symbol = commands
+        .spawn((
+            ImageNode::new(loot_symbol),
+            Node {
+                width: Val::Px(30.),
+                height: Val::Px(30.),
+                margin: UiRect::left(Val::Px(10.)),
+                ..default()
+            },
+        ))
+        .id();
+
+    let loot_text_container = commands
+        .spawn(Node {
+            width: Val::Px(80.),
+            height: Val::Percent(100.),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        })
+        .id();
+
+    commands.entity(loot_text_container).with_child((
+        CurrentLootText,
+        Text::default(),
+        TextFont {
+            font: spacey_font,
+            font_size: 40.,
+            ..default()
+        },
+        Node {
+            margin: UiRect::left(Val::Px(-5.)),
+            ..default()
+        },
+    ));
+
+    commands
+        .entity(loot_display)
+        .add_children(&[loot_symbol, loot_text_container]);
 }
 
 /// Update the loot display
-fn update_loot_display(loot: Res<Loot>, mut text: Single<&mut Text, With<LootDisplay>>) {
+fn update_loot_display(loot: Res<Loot>, mut text: Single<&mut Text, With<CurrentLootText>>) {
     text.0 = loot.to_string();
 }
